7c019f9cacc33a47339b36081f662fee5287b844
David Schaaf <david.p.schaaf@gmail.com>
2014-06-11 01:40:16 -0700

    cleaned up downpayment calculation

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -9,16 +9,12 @@ class LoansController < ApplicationController
   def create
     @loan = Loan.new
     update_loan_attributes @loan, params
-    @loan.calculate_loan_details
-    @loan.save
     redirect_to edit_loan_path @loan
   end
 
   def update
     @loan = Loan.find params[:id]
     update_loan_attributes @loan, params
-    @loan.calculate_loan_details
-    @loan.save
     PdfGenerator.pre_approval_letter(@loan)
     LetterHandler.pre_approval_letter(@loan).deliver
     redirect_to root_path
@@ -27,8 +23,6 @@ class LoansController < ApplicationController
   def calculator
     @loan = Loan.find params[:id]
     update_loan_attributes @loan, params
-    @loan.calculate_loan_details
-    @loan.save
     render partial: 'calculator', locals: {loan: @loan}
   end
 
@@ -40,21 +34,14 @@ class LoansController < ApplicationController
 
   def update_loan_attributes loan, params
     loan.update_attributes sanitized_loan_params(params[:loan])
-    loan.update_attribute params[:downpayment][:type], sanitized_downpayment_params(params[:downpayment])
+    loan.calculate_loan_details params
+    loan.save
   end
 
   def sanitized_loan_params args
     args[:annual_gross_income] = sanitize_integer(args[:annual_gross_income])
     args[:monthly_debt] = sanitize_integer(args[:monthly_debt])
     args
-  end
-
-  def sanitized_downpayment_params args
-    if args[:type] == 'downpayment_amount'
-      sanitize_integer(args[:value])
-    else
-      sanitize_float(args[:value])
-    end
   end
 
 end
--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,19 +1,22 @@
 class Loan < ActiveRecord::Base
 
+  include ApplicationHelper
+
   attr_accessible :annual_gross_income, :monthly_debt, :downpayment_amount, :downpayment_percent, :fico, :state, :email, :full_name
 
-  def initialize
-    super
-    @interest_rate = 0.0425
-    @insurance_percent = 0.0035
-    @tax_rate = 0.0115 / 12
-    @number_of_months = 30 * 12
-    @monthly_interest_rate = @interest_rate / 12
+  after_initialize :set_defaults
+
+  def set_defaults
+    if new_record?
+      self.annual_gross_income = 70000
+      self.monthly_debt = 1000
+      self.downpayment_amount = 50000
+    end
   end
 
-  def calculate_loan_details
+  def calculate_loan_details args
     calc_max_home_amount
-    calc_downpayments
+    calc_downpayments args
     calc_loan_amount
     calc_monthly_payment
   end
@@ -26,15 +29,32 @@ class Loan < ActiveRecord::Base
     self.downpayment_amount.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
   end
 
+  def display_loan_amount
+    self.loan_amount.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
+  end
+
+  def display_annual_gross_income
+    self.annual_gross_income.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
+  end
+
+  def display_monthly_debt
+    self.monthly_debt.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
+  end
+
+  def calc_downpayments args
+    self.update_attribute args[:downpayment][:type], sanitized_downpayment_params(args[:downpayment])
+
+    if args[:downpayment][:type] == 'downpayment_amount'
+      calc_downpayment_percent
+    else
+      calc_downpayment_amount
+    end
+  end
+
   private
 
   def calc_max_home_amount
-    self.max_home_amount = (0.5 * self.annual_gross_income/12 - self.monthly_debt ) / ((@monthly_interest_rate / (1 - (1 + @monthly_interest_rate)**(-@number_of_months))) + @tax_rate + @insurance_percent)
-  end
-
-  def calc_downpayments
-    calc_downpayment_percent unless self.downpayment_percent
-    calc_downpayment_amount  unless self.downpayment_amount
+    self.max_home_amount = (0.5 * self.annual_gross_income/12 - self.monthly_debt ) / ((monthly_interest_rate / (1 - (1 + monthly_interest_rate)**(-number_of_months))) + tax_rate + insurance_percent)
   end
 
   def calc_downpayment_percent
@@ -71,6 +91,14 @@ class Loan < ActiveRecord::Base
 
   def monthly_interest_rate
     interest_rate / 12
+  end
+
+  def sanitized_downpayment_params args
+    if args[:type] == 'downpayment_amount'
+      sanitize_integer(args[:value])
+    else
+      sanitize_float(args[:value])
+    end
   end
 
 end
